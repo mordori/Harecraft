9672c329576d70ec2bb2b05abe989a69b112fc77
Mika <mika.ylipentti@gmail.com>
2018-02-23 11:39:39 +0200

    Initial commit

---
+++ android/src/fi/tamk/tiko/harecraft/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package fi.tamk.tiko.harecraft;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import fi.tamk.tiko.harecraft.GameMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameMain(), config);
+	}
+}
